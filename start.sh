c8aefb7c011f57ca1c6cadf8f80887a303f938c7
cjbates02 <christianbates02@gmail.com>
2025-01-28 16:20:00 -0500

    fixed image path conflict bug.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # docker buildx build --no-cache --platform linux/amd64 -t personal_website_frontend .
-docker build --no-cache -t cctadmin/cct_website .
-docker run -p 8080:80 cctadmin/cct_website
+docker build --no-cache -t cct_website .
+docker run -p 8080:80 cct_website
 # docker login
 # docker push cjbates02/personal_website_frontend
